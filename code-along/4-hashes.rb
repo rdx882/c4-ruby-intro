0d516b6dd4fb0de2f0acf6c95d1b68c0d8f9b496
Roland Deex <roland.deex@kellogg.northwestern.edu>
2025-02-01 15:33:04 +0000

    2

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -5,14 +5,40 @@
 
 
 
-# Hashes are lists of *key-value pairs*
-ben = {"name" => "Ben", "location" => "Chicago", "status" => "ENTR at Kellogg"}
-puts ben
+# # Hashes are lists of *key-value pairs*
+# ben = {"name" => "Ben", "location" => "Chicago", "status" => "ENTR at Kellogg"}
+# puts ben
 
 
-# Accessing data from the hash
+# # Accessing data from the hash
 
-location = ben["location"]
-puts location
+# location = ben["location"]
+# puts location
 
 # More Complex Hashes
+
+#similar to arrays
+
+# my_profile = {
+#     "name" => "Ben",
+#     "location" => {"city" => "Chicago", "state" => "Illinois"},
+#     "status" => "Kellogg"
+# }
+
+# # puts my_profile
+
+
+# my_profile["name"] = "ROland"
+
+# puts my_profile
+
+#hash with array inside of it (timeline is an array)
+
+complete_profile = {
+    "name" => "Ben",
+        "location" => { "city" => "Chicago", "state" => "Illinois" },
+        "timeline" => [{ "status" => "eating tacos", "posted" => "7:30am" },
+                        { "status" => "brushing teeth", "posted" => "8:00am"}] 
+}
+
+puts complete_profile["timeline"][0]["status"]
--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -2,10 +2,53 @@
 # is the same as where this file is located and then run:
 # ruby 5-loops.rb
 
+index = 0
+
 # Infinite Tacos -> Finite Tacos
 # loop do
+#     if index == 5
+#         break
+#     end
+
 #   puts "tacos!"
+
+# index = index + 1
+
 # end
 
-# Loop through tacos
+
+
+
+# # Loop through tacos
+
+
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+# loop do
+
+#     #stops loop if we reached the end
+#     if index == tacos.length
+#         break
+#     end
+
+#     #retrieves the variable taco = index position
+# taco = tacos[index]
+
+# #then puts tacos out into the world
+# puts "#{taco} tacos!"
+
+# #without this, infinite loop
+# index = index + 1
+
+# end
+
 # tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+
+#shorter way to do the above - use for.. in statement to both create a variable (you are naming each thing in the tacos array with a single variable) and put it out into the world
+for myfavtaco in tacos
+
+    #string interpolation using each item in the array
+    puts "#{myfavtaco} tacos!"
+    
+end
